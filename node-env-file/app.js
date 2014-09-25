16b87f42b2e67b71cfcafdb59949cf5ebe997782
Lance Ball <lball@redhat.com>
2014-09-25 13:22:01 -0400

    Add some output to the basic app

--- node-env-file/app.js
+++ node-env-file/app.js
@@ -9,8 +9,17 @@ assert.equal(process.env.FOO, "defaultfoo");
 assert.equal(process.env.BAR, "bar1");
 assert.equal(process.env.BAZ, "1");
 
+console.log(process.env.FOO);
+console.log(process.env.BAR);
+console.log(process.env.BAZ);
+
 // Load another ENV file - and overwrite any defined ENV variables.
 env(__dirname + '/.env2', {overwrite: true});
 assert.equal(process.env.FOO, "foo2");
 assert.equal(process.env.BAR, "bar2");
 assert.equal(process.env.BAZ, "2");
+
+console.log(process.env.FOO);
+console.log(process.env.BAR);
+console.log(process.env.BAZ);
+
