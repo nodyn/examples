ab9eea4f25035d444c4ebe8fe6a0746e6304b2d5
Lance Ball <lball@redhat.com>
2014-10-13 15:04:03 -0400

    Remove static file serving for the time being

--- express/app.js
+++ express/app.js
@@ -1,13 +1,10 @@
 var express = require('express'),
-    serveStatic = require('serve-static'),
     app = express();
-
-app.use(serveStatic('public', {'index': ['default.html', 'default.htm']}));
 
 app.get('/hello.txt', function(req, res){
   res.send('Hello World');
 });
 
 var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
+  console.log('Listening on port %d', server.address().port);
 });
