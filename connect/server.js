dd085df742277102c1acfd61c622b277c8d495f6
Lance Ball <lball@redhat.com>
2014-11-11 16:21:21 -0500

    Update connect.js example to include additional middleware.

--- connect/server.js
+++ connect/server.js
@@ -1,14 +1,33 @@
-var http         = require('http'),
-    connect      = require('connect'),
-    morgan       = require('morgan'),
-    port         = 3000;
+var connect = require('connect'),
+    http = require('http'),
+    port = 3000;
 
-var app = connect()
-  .use(morgan('combined'))
-  .use(function(req, res){
-    res.end('hello from Nodyn');
-  });
+var app = connect();
 
+// gzip/deflate outgoing responses
+var compression = require('compression')
+app.use(compression())
+
+
+// store session state in browser cookie
+var cookieSession = require('cookie-session');
+app.use(cookieSession({
+    keys: ['secret1', 'secret2']
+}));
+
+// parse urlencoded request bodies into req.body
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded());
+
+// respond to all requests
+app.use(function(req, res){
+  res.end('Hello from Nodyn!\n');
+});
+
+var morgan = require('morgan');
+app.use(morgan('combined'));
+
+//create node.js http server and listen on port
 http.createServer(app).listen(port, function() {
   console.log( "listening on " + port );
 });
